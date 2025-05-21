50d1b90477ea866f7cb9e1175cf393556e2123b8
OceanKang <14096666+oceankang@user.noreply.gitee.com>
2025-05-21 21:22:29 +0800

    update tools of identify gitee's repos

--- parse_repoweb.py
+++ parse_repoweb.py
@@ -5,7 +5,7 @@ import yaml
 from omegaconf import OmegaConf
 
 REPOPATH = "./repo_info"
-WEBPATH = "./repo_info/repo_web.json"
+WEBPATH = "./repo_info/repo_web_whole.json"
 YAMLPATH = "./configs"
 
 def load_repoweb(path:str):
@@ -13,36 +13,42 @@ def load_repoweb(path:str):
         data = json.load(f)
     return data
 
-def parse(url:str):
-    pattern = r"https://github\.com/([^/]+)/([^/]+)"
-    match = re.match(pattern, url)
-    if match:
-        owner = match.group(1)
-        project_name = match.group(2)
+def parse(url:str, key:str):
+    pattern_github = r"https://github\.com/([^/]+)/([^/]+)"
+    pattern_gitee = r"https://gitee\.com/([^/]+)/([^/]+)"
+    match_github = re.match(pattern_github, url)
+    match_gitee = re.match(pattern_gitee, url)
+    if match_github:
+        owner = match_github.group(1)
+        project_name = match_github.group(2)
+    elif match_gitee:
+        owner = match_gitee.group(1)
+        project_name = match_gitee.group(2)
     else:
         print("No match found")
     return_dict = dict()
     return_dict['owner'] = owner
     return_dict['project'] = project_name
+    return_dict['group'] = key
     return return_dict
 
 def writeconfig(cfgdict:dict):
     # open example config.yaml
-    cfg = OmegaConf.load(YAMLPATH + '\config.yaml')
+    cfg = OmegaConf.load(YAMLPATH + r'\template\config.yaml')
     # make a new dict for each repo'config.yaml
     res_dict = dict()
     res_dict['github'] = dict()
-    res_dict['github']['GITHUB_TOKEN'] = cfg.github.GITHUB_TOKEN
     res_dict['github']['PER_PAGE'] = cfg.github.PER_PAGE
     res_dict['github']['REPO_OWNER'] = cfgdict['owner']
     res_dict['github']['REPO_NAME'] = cfgdict['project']
-    with open(f"{YAMLPATH}/{cfgdict['project']}.yaml", "w") as file:
+    res_dict['github']['GROUP'] = cfgdict['group']
+    with open(f"{YAMLPATH}/tmp/{cfgdict['group']}.yaml", "w") as file:
         yaml.dump(res_dict, file, allow_unicode=True, sort_keys=False)
     return None
 
 def write_repo_list(repolist:list):
     try:
-        with open(REPOPATH+"/repolist.txt", 'w') as f:
+        with open(REPOPATH+"/repo_list.txt", 'w') as f:
             for item in repolist:
                 f.write(str(item) + '\n')
         print('Repo.txt writing successfully!')
@@ -54,7 +60,7 @@ def main():
     repolist = list()
     for idx, val in enumerate(webs_dict):
         web = webs_dict[val]
-        res = parse(web)
+        res = parse(web, val)
         writeconfig(res)
         repolist.append(res['project'])
     write_repo_list(repolist)
